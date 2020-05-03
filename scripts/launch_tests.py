6fff0c62e7f2fa05efb03b2adbb7ee4014dd3698
Amaury MAILLE <amaury.maille@ens-lyon.fr>
2020-05-03 11:33:42 +0200

    Add sequential locality to test runner
      * If several simulations must run on the same machine, sequential locality
    will ensure that they run one after another, while also allowing simulations
    to run in parallel across machines

--- scripts/launch_tests.py
+++ scripts/launch_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import asyncio
 import datetime
 import os
 import os.path
@@ -64,24 +65,49 @@ def decode_to_runs(data):
 
     return runs
 
+def generate_ssh_command_for(machine, simulation):
+    dirname = os.path.expanduser("~/NPB-sync") + "/{}.{}".format(machine, datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"))
+    ssh_command = "mkdir {} && cd {} && git clone git@github.com:amaurymaille/NPB-sync.git && cd NPB-sync && mkdir build && cd build && cmake -DSPDLOG_INCLUDE_DIR={} -DSPDLOG_LIBRARY={} -DCMAKE_ADDITIONAL_DEFINITIONS=".format(dirname, dirname, os.path.expanduser("~/NPB-sync/spdlog/include"), os.path.expanduser("~/NPB-sync/spdlog/build/libspdlog.a"))
+    if simulation._promise_kind == "active":
+        ssh_command += "-DACTIVE_PROMISES"
+
+    ssh_command += (" .. && cd ../scripts && python3 generate_dynamic_defines.py -w {} -x {} -y {} -z {} -l {} -f ../src/dynamic_defines.h && python3 launch_test.py -d {} -t {} " + " ".join([ "--" + sync for sync in simulation._synchronizers ])).format(simulation._dimw, simulation._dimx, simulation._dimy, simulation._dimz, simulation._loops, dirname + "/NPB-sync/build", simulation._threads)
+
+    return ssh_command
+ 
+def perform_simulation(machine, simulation):
+    ssh_command = generate_ssh_command_for(machine, simulation)
+    process = subprocess.Popen(["ssh", machine, ssh_command], shell=False)
+    return process
+
 def perform_run(run):
     machine = run._machine
     processes = []
 
     for simulation in run._simulations:
-        dirname = os.path.expanduser("~/NPB-sync") + "/{}.{}".format(machine, datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"))
-        ssh_command = "mkdir {} && cd {} && git clone git@github.com:amaurymaille/NPB-sync.git && cd NPB-sync && mkdir build && cd build && cmake -DSPDLOG_INCLUDE_DIR={} -DSPDLOG_LIBRARY={} -DCMAKE_ADDITIONAL_DEFINITIONS=".format(dirname, dirname, os.path.expanduser("~/NPB-sync/spdlog/include"), os.path.expanduser("~/NPB-sync/spdlog/build/libspdlog.a"))
-        if simulation._promise_kind == "active":
-            ssh_command += "-DACTIVE_PROMISES"
-
-        ssh_command += (" .. && cd ../scripts && python3 generate_dynamic_defines.py -w {} -x {} -y {} -z {} -l {} -f ../src/dynamic_defines.h && python3 launch_test.py -d {} -t {} " + " ".join([ "--" + sync for sync in simulation._synchronizers ])).format(simulation._dimw, simulation._dimx, simulation._dimy, simulation._dimz, simulation._loops, dirname + "/NPB-sync/build", simulation._threads)
-        
-        process = subprocess.Popen(["ssh", machine, ssh_command], shell=False)
-        processes += [ process ]
+        processes += [ perform_simulation(machine, simulation) ]
         time.sleep(1)
-        # print (ssh_command)
 
     return processes
+
+async def perform_simulation_async(machine, simulation):
+    ssh_command = generate_ssh_command_for(machine, simulation)
+    process = await asyncio.create_subprocess_exec("ssh", machine, ssh_command)
+    await process.wait()
+
+async def perform_run_async(run):
+    machine = run._machine
+
+    for simulation in run._simulations:
+        await perform_simulation_async(machine, simulation)
+
+async def perform_runs_local_sequentiality(runs):
+    simulations_coros = []
+
+    for run in runs:
+        simulations_coros += [ perform_run_async(run) ]
+
+    await asyncio.gather(*simulations_coros)
 
 def perform_runs(runs):
     all_processes = []
@@ -95,12 +121,16 @@ def perform_runs(runs):
 def main():
     parser = argparse.ArgumentParser(description="Launch sync tests on a cluster of machines")
     parser.add_argument("-f", "--file", help="Path to JSON file containing the simulations infos", required=True, type=argparse.FileType("r"))
+    parser.add_argument("-l", "--local-sequentiality", help="When several simulations must run on the same machine, run them sequentially instead of in parallel", action="store_true")
     args = parser.parse_args()
 
     result = json.load(args.file)
     runs = decode_to_runs(result["runs"])
 
-    perform_runs(runs)
+    if args.local_sequentiality:
+        asyncio.run(perform_runs_local_sequentiality(runs))
+    else:
+        perform_runs(runs)
 
 if __name__ == "__main__":
     main()
