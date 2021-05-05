50ced832bbe81f2d89a1036584889cf79e635279
Amaury MAILLE <amaury.maille@ens-lyon.fr>
2021-05-05 09:42:22 +0200

    Added synchronizers for LU...

--- src/lu/main.cpp
+++ src/lu/main.cpp
@@ -18,16 +18,22 @@
 namespace g = Globals;
 
 template<typename MatrixValue, DynamicStepPromiseMode mode>
-class LUSynchronizer : public Synchronizer<LUSolver> {
+class PromisePlusLUSynchronizer : public Synchronizer<LUSolver> {
 public:
-    LUSynchronizer(LUSolver const& m, Matrix2D& matrix, int nb_threads, DynamicStepPromiseBuilder<MatrixValue, mode> const& builder) : Synchronizer(m, matrix), _n_threads(nb_threads), _builder(builder) {
+    PromisePlusLUSynchronizer(LUSolver const& m, Matrix2D& matrix, int nb_threads, DynamicStepPromiseBuilder<MatrixValue, mode> const& builder) : Synchronizer(m, matrix), _n_threads(nb_threads), _builder(builder),
+        _result(boost::extents[g::LU::DIM][g::LU::DIM]) {
 
+    }
+
+    virtual void assert_okay() {
+        _m.assert_matrix_equals(_result, _m.get_expected());
     }
 
     template<typename F, typename... Args>
     void run(F&& f, Args&&... args) {
         (void)f;
-        kernel_lu_combine_n_pp(_matrix, _bs, _xs, _builder);
+        kernel_lu_combine_n_pp(_matrix, _result, _bs, _xs, _builder);
+        // kernel_lu_combine_n_omp(_matrix, _result, _bs, _xs);
     }
 
 private:
@@ -35,12 +41,108 @@ private:
     DynamicStepPromiseBuilder<MatrixValue, mode> const& _builder;
     std::vector<Vector1D> _xs;
     std::vector<Vector1D> _bs;
+    Matrix2D _result;
+};
+
+class OMPLU : public Synchronizer<LUSolver> {
+public:
+    OMPLU(LUSolver const& m, Matrix2D& matrix) : Synchronizer(m, matrix), 
+        _result(boost::extents[g::LU::DIM][g::LU::DIM])  { }
+
+    virtual void assert_okay() {
+        _m.assert_matrix_equals(_result, _m.get_expected());
+    }
+
+    template<typename F, typename... Args>
+    void run(F&& f, Args&&... args) {
+        kernel_lu_omp(_matrix, _result);
+    }
+
+private:
+    Matrix2D _result;
+};
+
+template<typename MatrixValue, DynamicStepPromiseMode mode>
+class PromisePlusLU : public Synchronizer<LUSolver> {
+public:
+    PromisePlusLU(LUSolver const& m, Matrix2D& matrix, DynamicStepPromiseBuilder<MatrixValue, mode> const& builder) :
+        Synchronizer(m, matrix), _builder(builder), _result(boost::extents[g::LU::DIM][g::LU::DIM]) { }
+
+    virtual void assert_okay() {
+        _m.assert_matrix_equals(_result, _m.get_expected());
+    }
+
+    template<typename F, typename... Args>
+    void run(F&& f, Args&&... args) {
+        std::vector<DynamicStepPromise<MatrixValue, mode>*> promises;
+        auto create_time = measure_time([&]() {
+            for (int i = 0; i < _matrix.size(); ++i)
+                promises.push_back(static_cast<DynamicStepPromise<Matrix2DValue, mode>*>(_builder.new_promise()));
+        });
+        std::cout << "Creation time: " << (double)create_time / BILLION << std::endl;
+        kernel_lu_omp_pp(_matrix, _result, promises);
+    }
+
+private:
+    DynamicStepPromiseBuilder<MatrixValue, mode> const& _builder;
+    Matrix2D _result;
+};
+
+class SequentialLUSynchronizer : public Synchronizer<LUSolver> {
+public:
+    SequentialLUSynchronizer(LUSolver const& m, Matrix2D& matrix) : Synchronizer(m, matrix), 
+        _result(boost::extents[g::LU::DIM][g::LU::DIM])  { }
+
+    virtual void assert_okay() {
+        _m.assert_matrix_equals(_result, _m.get_expected());
+    }
+
+    template<typename F, typename... Args>
+    void run(F&& f, Args&&... args) {
+        (void)f;
+        kernel_lu(_matrix, _result);
+        /* std::cout << "=== EXPECTED ===" << std::endl;
+        print_matrix(_m.get_expected());
+        std::cout << "=== RESULT ===" << std::endl;
+        print_matrix(_result); */
+    }
+
+protected:
+    Matrix2D _result;
+};
+
+class SequentialLUSolverSynchronizer : public SequentialLUSynchronizer {
+public:
+    SequentialLUSolverSynchronizer(LUSolver const& m, Matrix2D& matrix) : SequentialLUSynchronizer(m, matrix) {
+
+    }
+
+    virtual void assert_okay() {
+        _m.assert_matrix_equals(_result, _m.get_expected());
+    }
+
+    template<typename F, typename... Args>
+    void run(F&& f, Args&&... args) {
+        (void)f;
+        kernel_lu_combine_n(_matrix, _result, _bs, _xs);
+    }
+
+private:
+    std::vector<Vector1D> _xs;
+    std::vector<Vector1D> _bs;
 };
 
 class LUTimeCollector : public TimeCollector {
 public:
     void print_times() {
+        json runs_times;
+        json runs = json::array();
 
+        for (auto const& log: _times)
+            runs.push_back(log.get_json());
+
+        runs_times["runs"] = runs;
+        ExtraConfig::runs_times_file() << std::setw(4) << runs_times;
     }
 
     void print_iterations_times() {
@@ -48,35 +150,58 @@ public:
     }    
 
     void run_sequential(unsigned int iterations) {
+        TimeLog log("Sequential", "kernel_lu");
+        Matrix2D matrix(boost::extents[g::LU::DIM][g::LU::DIM]);
 
+        for (unsigned int i = 0; i < iterations; ++i) {
+            SequentialLUSolverSynchronizer sync(sLU, matrix);
+            add_time(log, i, measure_synchronizer_time(sync, []{}));
+        }
+
+        _times.push_back(log);
     }
 
     void run_array_of_promises(unsigned int iterations) {
+        for (unsigned int i = 0; i < iterations; ++i) {
 
+        }
     }
 
     void run_promise_of_array(unsigned int iterations) {
+        for (unsigned int i = 0; i < iterations; ++i) {
 
+        }
     }
 
     template<DynamicStepPromiseMode mode>
     void run_dsp(unsigned int iterations, unsigned int step, 
                  const std::string& synchronizer_name,
                  const std::string& function) {
+        std::cout << "Running DSP" << std::endl;
+
         unsigned int nb_threads = omp_nb_threads();
         TimeLog log(synchronizer_name, function);
+        TimeLog omp(synchronizer_name + "OMP", function);
+
         log.add_extra_arg("step", step);
 
         Matrix2D matrix(boost::extents[g::LU::DIM][g::LU::DIM]);
         DynamicStepPromiseBuilder<Matrix2D::element, mode> builder(g::LU::DIM, step, nb_threads);
 
         for (unsigned int i = 0; i < iterations; ++i) {
-            LUSynchronizer<Matrix2D::element, mode> sync(sLU, matrix, nb_threads, builder);
-            auto time = measure_synchronizer_time(sync, []{});
-            add_time(log, i, time);
+            // PromisePlusLUSynchronizer<Matrix2D::element, mode> sync(sLU, matrix, nb_threads, builder);
+            OMPLU omp_sync(sLU, matrix);
+            PromisePlusLU pp_sync(sLU, matrix, builder);
+
+            auto omp_time = measure_synchronizer_time(omp_sync, []{});
+            add_time(omp, i, omp_time);
+
+            auto pp_time = measure_synchronizer_time(pp_sync, []{});
+            add_time(log, i, pp_time);
         }
 
         _times.push_back(log);
+        _times.push_back(omp);
     }
 
     /* void run_dsp_prod_only(unsigned int iterations, unsigned int step) {
@@ -154,6 +279,7 @@ public:
 
     void process_run(unsigned int iterations, json run) override {
         std::string const& synchronizer = run[JSON::Run::synchronizer];
+        std::cout << "Processing run for synchronizer " << synchronizer << std::endl;
 
         auto get_step = [&]() -> unsigned int { 
             if (run.contains(JSON::Run::extras)) {
@@ -254,11 +380,13 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
+    log_general_data(DynamicConfig::_instance()._files.parameters_file());
+    sLU.init();
+    sLU.init_expected();
+
     LURunner runner(sDynamicConfigFiles.get_simulations_filename());
     runner.run();
     runner.dump();
-
-    log_general_data(DynamicConfig::_instance()._files.parameters_file());
 
     return 0;
 }
