fcbcb690f0d781f023f877ad6b04affe0d94eb57
Amaury MAILLE <amaury.maille@ens-lyon.fr>
2021-05-05 17:00:39 +0200

    Split step handling between producers and consumers

--- src/dedup/step.c
+++ src/dedup/step.c
@@ -1,17 +1,33 @@
 #include "step.h"
 
-void update_refine_step(unsigned int* step, unsigned int it) {
+void update_refine_insert_step(unsigned int* step, unsigned int it) {
 
 }
 
-void update_dedup_step(unsigned int* step, unsigned int it) {
+void update_refine_extract_step(unsigned int* step, unsigned int it) {
 
 }
 
-void update_compress_step(unsigned int* step, unsigned int it) {
+void update_dedup_insert_step(unsigned int* step, unsigned int it) {
 
 }
 
-void update_reorder_step(unsigned int* step, unsigned int it) {
+void update_dedup_extract_step(unsigned int* step, unsigned int it) {
+
+}
+
+void update_compress_insert_step(unsigned int* step, unsigned int it) {
+
+}
+
+void update_compress_extract_step(unsigned int* step, unsigned int it) {
+
+}
+
+void update_reorder_insert_step(unsigned int* step, unsigned int it) {
+
+}
+
+void update_reorder_extract_step(unsigned int* step, unsigned int it) {
 
 }
--- src/dedup/step.h
+++ src/dedup/step.h
@@ -1,16 +1,31 @@
 #pragma once
 
-/* When puhsing towards the refine queue / when pulling from the refine queue */
-void update_refine_step(unsigned int* step, unsigned int it);
-/* When pushing towards the deduplicate queue / pulling from the dedup queue */
-void update_dedup_step(unsigned int* step, unsigned int it);
-/* When pushing towards the compress queue / pulling from the compress queue */
-void update_compress_step(unsigned int* step, unsigned int it);
-/* When pushing towards the reorder queue / pulling from the reorder queue */
-void update_reorder_step(unsigned int* step, unsigned int it);
+/* When puhsing towards the refine queue */
+void update_refine_insert_step(unsigned int* step, unsigned int it);
+/* When pulling from the refine queue */
+void update_refine_extract_step(unsigned int* step, unsigned int it);
+/* When pushing towards the deduplicate queue */
+void update_dedup_insert_step(unsigned int* step, unsigned int it);
+/* When pulling from the deduplicate queue */
+void update_dedup_extract_step(unsigned int* step, unsigned int it);
+/* When pushing towards the compress queue */
+void update_compress_insert_step(unsigned int* step, unsigned int it);
+/* When pulling from the compress que */
+void update_compress_extract_step(unsigned int* step, unsigned int it);
+/* When pushing towards the reorder queue */
+void update_reorder_insert_step(unsigned int* step, unsigned int it);
+/* When pulling from the reorder queue */
+void update_reorder_extract_step(unsigned int* step, unsigned int it);
 
-inline unsigned int refine_initial_step() { return 1; }
-inline unsigned int dedup_initial_step() { return 10; }
-inline unsigned int compress_initial_step() { return 20; }
-inline unsigned int reorder_initial_step() { return 30; }
+inline unsigned int refine_initial_insert_step() { return 1; }
+inline unsigned int refine_initial_extract_step() { return 1; }
+
+inline unsigned int dedup_initial_insert_step() { return 10; }
+inline unsigned int dedup_initial_extract_step() { return 10; }
+
+inline unsigned int compress_initial_insert_step() { return 20; }
+inline unsigned int compress_initial_extract_step() { return 20; }
+
+inline unsigned int reorder_initial_insert_step() { return 30; }
+inline unsigned int reorder_initial_extract_step() { return 30; }
 
