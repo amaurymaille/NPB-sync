e559013bb279e195a23a04b2d1a91ab068314c2e
Amaury MAILLE <amaury.maille@ens-lyon.fr>
2021-05-05 09:34:29 +0200

    Add an OpenMP pragma critical FIFO

--- src/fifo.h
+++ src/fifo.h
@@ -1,8 +1,12 @@
 #ifndef FIFO_H
 #define FIFO_H
 
+#include <cstdio>
+
+#include <atomic>
 #include <condition_variable>
 #include <mutex>
+#include <omp.h>
 #include <queue>
 #include <utility>
 
@@ -15,15 +19,6 @@ public:
         std::lock_guard<std::mutex> lck(_m);
         _queue.push(t);
         _empty.notify_all();
-    }
-
-    void pop(T&& t) {
-        std::unique_lock<std::mutex> lck(_m);
-        while (_queue.empty())
-            _empty.wait(lck);
-
-        t = std::move(_queue.front());
-        _queue.pop();
     }
 
     T pop() {
@@ -40,6 +35,40 @@ private:
     std::mutex _m;
     std::condition_variable _empty;
     std::queue<T> _queue;
+};
+
+template<typename T>
+class omp_fifo {
+public:
+    omp_fifo() { _n.store(0, std::memory_order_relaxed); }
+
+    void push(T const& t) {
+        #pragma omp critical(queue)
+        {
+            _queue.push(t);
+        }
+
+        _n++;
+    }
+
+    T pop() {
+        while (_n.load(std::memory_order_acquire) == 0)
+            ;
+
+        T t;
+        #pragma omp critical(queue)
+        {
+            t = _queue.front();
+            _queue.pop();
+        }
+
+        _n--;
+        return t;
+    }
+
+private:
+    std::queue<T> _queue;
+    std::atomic<int> _n;
 };
 
 #endif // FIFO_JH
