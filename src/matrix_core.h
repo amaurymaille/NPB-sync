2b54662d94f374ee98af276a09ee226fc98f8281
Amaury MAILLE <amaury.maille@ens-lyon.fr>
2021-05-05 09:36:02 +0200

    Removed some pure virtual functions from MatrixCore for some reason

--- src/matrix_core.h
+++ src/matrix_core.h
@@ -12,7 +12,7 @@ public:
     virtual void init() = 0;
     // Assert that the matrix other was properly initialized by comparing it
     // with our own initialized matrix
-    virtual void assert_okay_init(const Matrix& other) const = 0;
+    // virtual void assert_okay_init(const Matrix& other) const = 0;
 
     // If a filename is provided on the CLI, init the expected matrix from this
     // file. Otherwise, init with default values and compute the expected
@@ -20,17 +20,17 @@ public:
     virtual void init_expected() = 0;
     // Assert that the matrix other was properly computed by comparing it
     // with our own computed matrix
-    virtual void assert_okay_expected(const Matrix& other) const = 0;
+    // virtual void assert_okay_expected(const Matrix& other) const = 0;
 
     // Init the initial matrix from other
-    virtual void init_from(Matrix const& other) = 0;
+    // virtual void init_from(Matrix const& other) = 0;
     // Init the expected matrix from other
-    virtual void init_expected_from(Matrix const& other) = 0;
+    // virtual void init_expected_from(Matrix const& other) = 0;
 
     // Assert that the matrix other is equal to the initialized matrix
-    virtual void assert_equals(Matrix const& other) const = 0;
+    // virtual void assert_equals(Matrix const& other) const = 0;
     // Assert that the matrix other is equal to the computed matrix
-    virtual void assert_expected_equals(Matrix const& other) const = 0;
+    // virtual void assert_expected_equals(Matrix const& other) const = 0;
 
     // Read-access to the initialized matrix
     const Matrix& get_matrix() const { return _matrix; }
